9f133b268359768be0eecb9ed8c29cc34863fa47
Lucas Lois <lucasloisp@gmail.com>
2021-12-24 18:49:46 -0300

    feat: adds e1rm related views to the database

--- db/init.sql
+++ db/init.sql
@@ -8,3 +8,19 @@ CREATE TABLE IF NOT EXISTS exercise_set
     rpe           REAL,
     PRIMARY KEY (exercise_name, set_date, set_order)
 );
+
+CREATE VIEW exercise_set_e1rm AS
+SELECT s.*,
+       rp.percentage,
+       s.weight * 100 / rp.percentage AS e1rm
+FROM exercise_set s
+         LEFT JOIN rpe_percentage rp on s.reps = rp.reps AND s.rpe = rp.rpe
+
+CREATE VIEW daily_1rm_per_exercise AS
+SELECT es.set_date, es.exercise_name, es.e1rm
+FROM exercise_set_e1rm es
+         INNER JOIN (
+    SELECT esj.set_date, esj.exercise_name, MAX(esj.e1rm) as e1rm
+    FROM exercise_set_e1rm esj
+    GROUP BY (esj.set_date, esj.exercise_name)
+) esj ON esj.set_date = es.set_date AND esj.exercise_name = es.exercise_name AND esj.e1rm = es.e1rm
